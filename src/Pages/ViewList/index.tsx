7b5a7422b0c074abd7c996d06f74fec8e8c0fef9
Brian Christopher Mendoza <78596685+briancgmendoza@users.noreply.github.com>
2021-08-29 06:54:31 +0800

    Added quick fix

--- src/Pages/ViewList/index.tsx
+++ src/Pages/ViewList/index.tsx
@@ -6,13 +6,14 @@ import useHttp from "../../Hooks/use-http";
 import { getData, addData, deleteEmployee } from "../../Lib/api";
 
 // Importing Re-usesable Components
-import Input from "../../Components/Input";
 import Button from "../../Components/Button";
 import Spinner from "../../Components/Spinner";
 
 // Importing Bootstrap Icons .svg file
 import Pencil from "../../Asset/pencil-icon.svg";
 import Delete from "../../Asset/delete-icon.svg";
+import Modal from "../../Components/Modal";
+import Form from "../../Components/Form";
 
 const ViewList = () => {
   const [lastName, setLastName] = useState("");
@@ -39,30 +40,6 @@ const ViewList = () => {
   if (error) {
     return <p className="d-flex justify-content-center m-3">{error}</p>;
   }
-
-  // Add Request
-  const handleAdd = ({
-    lastName,
-    firstName,
-    middleName,
-    age,
-    address,
-    phoneNumber,
-  }: any) => {
-    addData({ lastName, firstName, middleName, age, address, phoneNumber });
-  };
-
-  // Once Delete icon is clicked, delete the user in the db depending on its EmployeeMasterID
-  const handleDelete = (id: any) => {
-    if (window.confirm("Are you sure?")) {
-      deleteEmployee(id);
-    }
-
-    /* In C++, return 0 means it was successful and it did what it was intended to do. 
-               Not sure if its the same in JS, but I prefer returning 0 or putting return after if(){}    
-    */
-    return 0;
-  };
 
   return (
     <section>
@@ -114,7 +91,13 @@ const ViewList = () => {
 
                   <Button
                     type="button"
-                    onClick={() => handleDelete(emp.EmployeeMasterID)}
+                    onClick={() => {
+                      if (window.confirm("Are you sure?")) {
+                        deleteEmployee(emp.EmployeeMasterID);
+                      }
+                      /* In C++, return 0 means it was successful and it did what it was intended to do. Not sure if its the same in JS, but I prefer returning 0 or putting return after if(){} */
+                      return 0;
+                    }}
                     className="btn btn-light mr-1"
                   >
                     <img src={Delete} alt="Delete" />
@@ -130,127 +113,34 @@ const ViewList = () => {
         </tbody>
       </table>
 
-      <section
-        className="modal fade"
-        id="exampleModal"
-        tabIndex={-1}
-        aria-hidden="true"
-      >
-        <div className="modal-dialog modal-lg modal-dialog-centered">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title">Add Employee</h5>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
-            </div>
-
-            <div className="modal-body">
-              <div className="d-flex flex-row bd-highlight mb-3">
-                <div className="p-2 w-100 bd-highlight">
-                  <div className="input-group mb-3">
-                    <span className="input-group-text">Last Name</span>
-                    <Input
-                      type="text"
-                      className="form-control"
-                      placeholder="Ex: Dela Cruz"
-                      value={lastName}
-                      onChange={(e: React.FormEvent<HTMLInputElement>) =>
-                        setLastName(e.currentTarget.value)
-                      }
-                    />
-                  </div>
-
-                  <div className="input-group mb-3">
-                    <span className="input-group-text">First Name</span>
-                    <Input
-                      type="text"
-                      className="form-control"
-                      placeholder="Ex: Juan"
-                      value={firstName}
-                      onChange={(e: React.FormEvent<HTMLInputElement>) =>
-                        setFirstName(e.currentTarget.value)
-                      }
-                    />
-                  </div>
-
-                  <div className="input-group mb-3">
-                    <span className="input-group-text">Middle Name</span>
-                    <Input
-                      type="text"
-                      className="form-control"
-                      placeholder="Ex: Gervasio"
-                      value={middleName}
-                      onChange={(e: React.FormEvent<HTMLInputElement>) =>
-                        setMiddleName(e.currentTarget.value)
-                      }
-                    />
-                  </div>
-
-                  <div className="input-group mb-3">
-                    <span className="input-group-text">Age</span>
-                    <Input
-                      type="text"
-                      className="form-control"
-                      placeholder="Ex: 20"
-                      value={age}
-                      onChange={(e: React.FormEvent<HTMLInputElement>) =>
-                        setAge(e.currentTarget.value)
-                      }
-                    />
-                  </div>
-
-                  <div className="input-group mb-3">
-                    <span className="input-group-text">Address</span>
-                    <Input
-                      type="text"
-                      className="form-control"
-                      placeholder="Ex: Metro Manila, Philippines"
-                      value={address}
-                      onChange={(e: React.FormEvent<HTMLInputElement>) =>
-                        setAddress(e.currentTarget.value)
-                      }
-                    />
-                  </div>
-
-                  <div className="input-group mb-3">
-                    <span className="input-group-text">Phone Number</span>
-                    <Input
-                      type="text"
-                      className="form-control"
-                      placeholder="Ex: 09123456789"
-                      value={phoneNumber}
-                      onChange={(e: React.FormEvent<HTMLInputElement>) =>
-                        setPhoneNumber(e.currentTarget.value)
-                      }
-                    />
-                  </div>
-
-                  <button
-                    type="button"
-                    className="btn btn-primary float-end"
-                    onClick={() =>
-                      handleAdd({
-                        lastName,
-                        firstName,
-                        middleName,
-                        age,
-                        address,
-                        phoneNumber,
-                      })
-                    }
-                  >
-                    Create
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Modal modalName="Add Employee">
+        <Form
+          // FIXME: Look for an alternative way to pass multiple props
+          lastName={lastName}
+          setLastName={setLastName}
+          firstName={firstName}
+          setFirstName={setFirstName}
+          middleName={middleName}
+          setMiddleName={setMiddleName}
+          age={age}
+          setAge={setAge}
+          address={address}
+          setAddress={setAddress}
+          phoneNumber={phoneNumber}
+          setPhoneNumber={setPhoneNumber}
+          onSubmit={(e: React.SyntheticEvent) => {
+            e.preventDefault();
+            addData({
+              lastName,
+              firstName,
+              middleName,
+              age,
+              address,
+              phoneNumber,
+            });
+          }}
+        />
+      </Modal>
     </section>
   );
 };
